0498853e17be72ac02cd1dd787896fb8b7423a2f
Md. Al-Amin <mdalaminh052@gmail.com>
2024-03-31 13:06:19 +0600

    footer sectios implemented

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,9 @@
-const SectionTitle = ({ title }) => {
+const SectionTitle = ({ title, className }) => {
 	return (
 		<div>
-			<h1 className="text-center w-fit mx-auto pb-5 px-6 m-8 mb-24 md:mb-32 text-textPrimary border-b-[5px] text-5xl md:text-6xl font-bold rounded-3xl">
+			<h1
+				className={`text-center w-fit mx-auto pb-5 px-6 m-8 mb-24 md:mb-32 text-textPrimary border-b-[5px] text-5xl md:text-6xl font-bold rounded-3xl ${className}`}
+			>
 				{title}
 			</h1>
 		</div>
